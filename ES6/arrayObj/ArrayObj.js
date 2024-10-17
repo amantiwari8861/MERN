69aeb2a4524f46850760ec0d63110bf64d362239
Aman Tiwari <amantiwari8861@gmail.com>
2024-10-17 15:29:31 +0530

    array and objects

---
+++ ES6/arrayObj/ArrayObj.js
@@ -0,0 +1,42 @@
+let users =[
+    {
+        id: 101,
+        name: "Aman Tiwari",
+        age: 25,
+        skills: ["C", "C++", "MEAN", "MERN", "JAVA FSD", "etc"],
+        address: {
+            city: "gurugram",
+            pincode: 122001
+        }
+    }, 
+    {
+        id: 102,
+        name: "naveen ",
+        age: 25,
+        skills: ["C", "C++", "MEAN", "MERN"],
+        address: {
+            city: "gurugram",
+            pincode: 122001
+        }
+    }, 
+    {
+        id: 103,
+        name: "Tanishq",
+        age: 25,
+        skills: ["C", "C++", "MEAN", "MERN", "JAVA FSD", "etc"],
+        address: {
+            city: "gurugram",
+            pincode: 122001
+        }
+    }
+];
+
+// Array of objects
+// console.log(users);
+// console.log(users[1]);
+
+// REST API 
+// https://freeapi.miniprojectideas.com/api/clientstrive/getAllRoles
+
+// H.w Array methods 
+
