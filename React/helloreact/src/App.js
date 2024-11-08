d93903c282c1111c9bfc0c1854e1758afd1d03f0
Aman Tiwari <amantiwari8861@gmail.com>
2024-11-08 15:41:26 +0530

    first react app

--- React/helloreact/src/App.js
+++ React/helloreact/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+// function App() {
+//   return (<h1>Hello World in React</h1>);
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// JSX -> babel(transpiler) -> js
+
+let App = () =>
+(
+  <div>
+    <button>click me </button>
+    <h1 className='bg-primary'>Hello World in React</h1>
+  </div>
+);
 
 export default App;
