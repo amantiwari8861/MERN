087b9f98808cb19984c9bf2d8020545a89ae54cd
Aman Tiwari <amantiwari8861@gmail.com>
2024-11-20 16:44:00 +0530

    tailwind and rippleui

---
+++ React/onlinebank/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Sidebar from './components/Sidebar'
+// import StoreNav from './components/StoreNav'
+// import Hello from './components/hello'
+
+function App() {
+  return (
+    <>
+      {/* <StoreNav/> */}
+      <Sidebar />
+      {/* <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      <Hello/> */}
+    </>
+  )
+}
+
+export default App
---
+++ React/onlinebank/src/components/Hello.jsx
@@ -0,0 +1,9 @@
+const Hello = () => {
+    return (
+        <div>
+            <h1>Good Morning!</h1>
+        </div>
+    )
+}
+
+export default Hello
